846a4d17a23ab49529bd91b27c1a4efd712939bd
Xavier Perez <xperez007@gmail.com>
2020-02-12 21:04:17 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,3 +1,14 @@
+def hello(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
+
+
 require 'pry'
 
 def hello(array)
